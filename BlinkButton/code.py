5cfe83f8e4b0bb3efe96ffd084fff67dbe5db983
R Gary Cutbill <gary.cutbill@gmail.com>
2026-04-09 15:17:22 -0400

    Adding BlinkButton and updating Blink

--- BlinkButton/code.py
+++ BlinkButton/code.py
@@ -10,6 +10,13 @@ from time import sleep
 ledPin = board.GP16
 led = neopixel.NeoPixel(ledPin, 1, brightness=.2)
 
+# Extra setup for a switch connected between Pin 14 and Ground
+import digitalio
+buttonPin = board.GP14
+button = digitalio.DigitalInOut(buttonPin)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
 # This is defining some basic colors
 RED=(255,0,0)
 GREEN=(0,255,0)
@@ -19,7 +26,12 @@ BLACK=(0,0,0)
 
 
 while True:
-    led.fill(RED)
+    # Choose to RED or GREEN depending on whether the button is pressed
+    if button.value: # The button is not pressed
+        led.fill(RED)
+    else: # The button is pressed
+        led.fill(GREEN)
+
     sleep(0.5)
     led.fill(WHITE)
     sleep(0.5)
